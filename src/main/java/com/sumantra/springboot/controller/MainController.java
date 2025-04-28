16a56d3e986e9d82022233de95ad6c270ceebacb
sumantra.chatterjee <sumantra.chatterjee@clario.com>
2025-04-28 11:56:45 +0530

    Commit MainController

--- src/main/java/com/sumantra/springboot/controller/MainController.java
+++ src/main/java/com/sumantra/springboot/controller/MainController.java
@@ -31,4 +31,30 @@ public class MainController {
         return userRepository.findAll();
     }
 
+    @GetMapping(path = "/{id}")
+    public @ResponseBody ResponseEntity<User> getUser(@PathVariable("id") int id){
+        if(userRepository.findById(id).isPresent()){
+            return new ResponseEntity<>(userRepository.findById(id).get(), HttpStatus.OK);
+        }
+        else{
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PutMapping(path = "/{id}")
+    public @ResponseBody ResponseEntity<User> updateUser(@PathVariable("id") int id, @RequestParam("name") String name, @RequestParam("email") String email){
+        User user = new User();
+        user.setName(name);
+        user.setEmail(email);
+        user.setId(id);
+        userRepository.save(user);
+        return new ResponseEntity<>(user, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public @ResponseBody ResponseEntity<User> deleteUser(@PathVariable("id") int id){
+        userRepository.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
 }
